01b7525f43188f3689b7f9643372baa47f0d00db
Calum Towler <calumnatowler@hotmail.co.uk>
2019-03-27 00:45:48 +0000

    Removed unnecessary bracketing so extracting from Procstockdata is simpler. Added user input for data call.

--- WebExtract/TradingProgram.py
+++ WebExtract/TradingProgram.py
@@ -4,7 +4,8 @@ First trial of alpha vantage. (It works, were sticking with this for the moment!
 23/03/19: initial commit
 25/03/19: Finally did something! Program can call intraday data for a tickr, and pull out the values. This is the
           beginning of manipulatable data.
-25/03/19: Introducing Stock class. Overarching class that calls intraday data from stocks in list. 
+25/03/19: Introducing Stock class. Overarching class that calls intraday data from stocks in list.
+26/03/19: Removed unnecessary bracketing so extracting from Procstockdata is simpler. Added user input for data call.
 Author: Calum Towler
 """
 from alpha_vantage.timeseries import TimeSeries
@@ -14,7 +15,7 @@ from alpha_vantage.timeseries import TimeSeries
 class Stock():
     
     def __init__(self):
-        self.Stocks = {'MSFT':'Microsoft'}#,'GOOGL':'Google','NKE':'Nike'}
+        self.Stocks = {'MSFT':'Microsoft','GOOGL':'Google'}#,'NKE':'Nike'}
         self.StockIndexes = []
         for i in self.Stocks.keys():
             self.StockIndexes.append(i)
@@ -22,24 +23,24 @@ class Stock():
     def collect_intraday_data(self):
         MyKey ='28M2VQTADUQ0HSCP'
         ts = TimeSeries(key=MyKey)
-        Output = []
+        Procstockdata = []
+#       InputTime = input("Which time interval? Options are '1min', '5min', '15min', '30min', '60min'")
+#       InputLength = input("Which time interval? Options are 'compact'or'full'.")
         for i in self.StockIndexes:
-            Procstockdata = []
-            stockdata, meta_stockdata = ts.get_intraday(i,'1min','full')
+            stockdata, meta_stockdata = ts.get_intraday(i,'1min','compact')
             Indexes = []
-            Output.append(i)
+            Procstockdata.append(i)
             for i in stockdata.keys():
                 Indexes.append(i)
             for item in Indexes:
                 get_interval = stockdata.get(item)
                 interval_data = [item, float(get_interval.get('1. open')), float(get_interval.get('4. close')), float(get_interval.get('3. low')), float(get_interval.get('2. high')), int(get_interval.get('5. volume'))]
-                Procstockdata.append([interval_data[0], interval_data[1], interval_data[2], interval_data[3], interval_data[4], interval_data[5]])
-                Output.append(Procstockdata)
-                Procstockdata = []
-        print(Output)
-        
-#Stocks = Stock()
+                Procstockdata.append([interval_data[0], interval_data[1], interval_data[2], interval_data[3], interval_data[4], interval_data[5]]) 
+        return Procstockdata 
+    
+Stocks = Stock()
 StockData = Stocks.collect_intraday_data()
+print(StockData)
 
 
 #MyKey ='28M2VQTADUQ0HSCP'
