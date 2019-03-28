f0b03038f6d06610465b7b6a25158717f1091dcc
Calum Towler <calumnatowler@hotmail.co.uk>
2019-03-28 01:33:29 +0000

    added write to file so stock data library can be built to train neural network

--- WebExtract/TradingProgram.py
+++ WebExtract/TradingProgram.py
@@ -6,16 +6,18 @@ First trial of alpha vantage. (It works, were sticking with this for the moment!
           beginning of manipulatable data.
 25/03/19: Introducing Stock class. Overarching class that calls intraday data from stocks in list.
 26/03/19: Removed unnecessary bracketing so extracting from Procstockdata is simpler. Added user input for data call.
+27/03/19: Added functionality to write data to files to build library to train neural network.
 Author: Calum Towler
 """
 from alpha_vantage.timeseries import TimeSeries
+from datetime import datetime
 
 #List of Stocks. Implement later once functions work.
 
 class Stock():
     
     def __init__(self):
-        self.Stocks = {'MSFT':'Microsoft','GOOGL':'Google'}#,'NKE':'Nike'}
+        self.Stocks = {'MSFT':'Microsoft','GOOGL':'Google','NKE':'Nike'}
         self.StockIndexes = []
         for i in self.Stocks.keys():
             self.StockIndexes.append(i)
@@ -27,7 +29,7 @@ class Stock():
 #       InputTime = input("Which time interval? Options are '1min', '5min', '15min', '30min', '60min'")
 #       InputLength = input("Which time interval? Options are 'compact'or'full'.")
         for i in self.StockIndexes:
-            stockdata, meta_stockdata = ts.get_intraday(i,'1min','compact')
+            stockdata, meta_stockdata = ts.get_intraday(i,'1min','full')
             Indexes = []
             Procstockdata.append(i)
             for i in stockdata.keys():
@@ -38,10 +40,18 @@ class Stock():
                 Procstockdata.append([interval_data[0], interval_data[1], interval_data[2], interval_data[3], interval_data[4], interval_data[5]]) 
         return Procstockdata 
     
+    def write_stockdata_library(self):
+       Datetime = datetime.now()
+       Datetime_str = Datetime.strftime("%m-%d-%Y_%H-%M-%S")
+       Test_Filename = 'StockData_'+str(Datetime_str)+'.txt'
+       with open('C:\\Users\Calum\Trading Program\TradingProgram\WebExtract\StockData\\'+Test_Filename, 'w') as f:
+           for item in StockData:
+               f.write("%s\n" % item)
+    
 Stocks = Stock()
 StockData = Stocks.collect_intraday_data()
-print(StockData)
-
+Stocks.write_stockdata_library()
+#print(StockData)
 
 #MyKey ='28M2VQTADUQ0HSCP'
 #ts = TimeSeries(key=MyKey)
