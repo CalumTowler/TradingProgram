716709983c24ee0be4c802f2ee12eabed3d98f5d
Calum Towler <calumnatowler@hotmail.co.uk>
2019-04-01 01:10:43 +0100

    tidying up

--- Stock.py
+++ Stock.py
@@ -31,14 +31,14 @@ class Stock():
         return stockdata
     
     def collect_daily_data(self):
-        stockdata, meta_stockdata = ts.get_daily(self.ticker,'1min','full')
+        stockdata, meta_stockdata = ts.get_daily(self.ticker,'full')
         Datetime = datetime.now()
         Datetime_str = Datetime.strftime("%m-%d-%Y_%H-%M-%S")
         Filename = str(self.ticker)+str(Datetime_str)+'Daily20Year.csv'
         FilePath = 'C:\\Users\Calum\Trading Program\TradingProgram\WebExtract\StockData' + '\\' + str(self.ticker) + '\\'
         stockdata.to_csv(FilePath+Filename)
         stockdata['4. close'].plot()
-        Title = 'Daily Times Series for the '+str(self.ticker)+' stock (1 min)'
+        Title = 'Daily Times Series for the '+str(self.ticker)+' stock'
         plt.title(Title)
         plt.show()
         return stockdata
