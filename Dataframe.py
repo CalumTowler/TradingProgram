07c98b4199cd20a769ebadca73ece572aed12cae
Alex Savarani <alex.sav@hotmail.co.uk>
2019-04-30 00:15:22 +0100

    daycheckerclean

--- Dataframe.py
+++ Dataframe.py
@@ -24,25 +24,23 @@ ts = TimeSeries(key=MyKey, output_format='pandas')
 def Day_Checker():
 
     Today = datetime.datetime.now()
-    Yesterday = Today - timedelta(1)
-    Yesterday = datetime.datetime.weekday(Yesterday) #returns day of week mon=0....sun=6
+    Todayday = datetime.datetime.weekday(Today)
 
-    if Yesterday==6:    #when day = sunday 
-        n=3 #makes yeserday friday 
-        Today = datetime.datetime.now()
+    if Todayday==0:    
+        n=3
+        Today_str = Today.strftime("%Y-%m-%d")
         Yesterday = Today - timedelta(n)    
         Yesterday_str = Yesterday.strftime("%Y-%m-%d")
     else:
         n=1
-        Today = datetime.datetime.now()
+        Today_str = Today.strftime("%Y-%m-%d")
         Yesterday = Today - timedelta(n)
         Yesterday_str = Yesterday.strftime("%Y-%m-%d")
     
     
-    return Yesterday_str
-    
-Yesterday_str=Day_Checker()
-print(Yesterday_str)
+    return Todayday, Today_str, Yesterday_str 
+
+
 
 class Stock():
     
@@ -52,23 +50,24 @@ class Stock():
     #first pull of full 5 day 1min data to create primary library 
     def initial_pull(self):
         stockdata, meta_stockdata = ts.get_intraday(self.ticker,'1min','full')
-        Filename = str(self.ticker) + str(Yesterday_str)+'.csv'
+        Filename = str(self.ticker) + str(Day_Checker()[2])+'.csv'
         FilePath = str(self.path)
         stockdata.to_csv(FilePath+Filename)
         
     def prilib(self):
         #make dataframe of 5 day file, change data type from float64 to float 32 to substantially save memory (standard is float64)
-        M1PL=pd.read_csv(str(self.path) + str(self.ticker)+str(Yesterday_str)+'.csv',
+        M1PL=pd.read_csv(str(self.path) + str(self.ticker)+str(Day_Checker()[2])+'.csv',
                          dtype={'1. open':np.float32,'2. high':np.float32, '3. low':np.float32, '4. close':np.float32, '5. volume':np.int})
         #remove days that are not today (to save 80% od data as each full pull gives 5 days )
-        M1PL=M1PL[M1PL['date'].str.contains(Yesterday_str)]#makes primary library of only yesterdays data 
-        print(M1PL)
+        M1PL=M1PL[M1PL['date'].str.contains(Day_Checker()[2])]#makes primary library of only yesterdays data 
         #make usable primary library of today and make excel file whcih replaces old file 
         M1PL=M1PL.sort_values(by='date',ascending=False) #changes date order
         M1PL=M1PL.set_index('date')
         print(M1PL)
-        M1PL.to_csv(str(self.path)+str(self.ticker)+str(Yesterday_str)+'.csv')
+        M1PL.to_csv(str(self.path)+str(self.ticker)+str(Day_Checker()[2])+'.csv')
         return M1PL      
+    
+        #this newdata pull will pull data every min and update the primary library with this new data  
     
  
 
