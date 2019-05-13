e17baec44d302bc3f0a07c273b86c6ba84a74394
Alex Savarani <alex.sav@hotmail.co.uk>
2019-05-13 01:03:10 +0100

    checking

--- Dataframeupdater.py
+++ Dataframeupdater.py
@@ -17,26 +17,63 @@ from datetime import timedelta
 import time
 import Dataframe
 from Dataframe import Day_Checker
+from Dataframe import Stock
+import threading 
 
+def first():
+    global x
+    x=1
+    print(x)
+    return x
+
+def repeat():
+    global x
+    x=x+1
+    print(x)
+    return x
+
+def Day_Checker():
+
+    Today = datetime.datetime.now()
+    Todayday = datetime.datetime.weekday(Today)
+
+    if Todayday==0:    
+        n=3
+        Today_str = Today.strftime("%Y-%m-%d")
+        Yesterday = Today - timedelta(n)    
+        Yesterday_str = Yesterday.strftime("%Y-%m-%d")
+    else:
+        n=1
+        Today_str = Today.strftime("%Y-%m-%d")
+        Yesterday = Today - timedelta(n)
+        Yesterday_str = Yesterday.strftime("%Y-%m-%d")
+    
+    
+    return Todayday, Today_str, Yesterday_str 
+
+
+print(Day_Checker()[0])
 
 def Oracle_Run():
-    start_time = datetime.time(23,46, 50)#would be 14:30:00 (9:30am(EST) US Market open)
-    endtime = datetime.time(23, 47, 10)#would be 21:00:00 (4:00pm(EST) US Market Close)
+    
+    start_time = datetime.time(1,14, 10)#would be 14:30:00 (9:30am(EST) US Market open)
+    endtime = datetime.time(1, 14, 30)#would be 21:00:00 (4:00pm(EST) US Market Close)
 
     Market_Open = start_time.isoformat(timespec='seconds')
     Market_Close  = endtime.isoformat(timespec='seconds')
 
     while True:
-        if Day_Checker()[0]==range(0,5) :#first checks that the day is mon-fri
-            if ((datetime.datetime.now().time().isoformat(timespec='seconds'))>Market_Open and #then checks the time is during market hours 
-                (datetime.datetime.now().time().isoformat(timespec='seconds')) < Market_Close): 
-                print('The Market is Open')
-                time.sleep(2)
-            else:
-                
-                print('The Market is Closed')
-                time.sleep(2)
-                #this elif statement checks whether the market is open    
+        if Day_Checker()[0]==range(5):#first checks that the day is mon-fri
+          print('m')
+          f=threading.Thread(target=first(), name ='t') 
+           
+          while ((datetime.datetime.now().time().isoformat(timespec='seconds'))>Market_Open and #then checks the time is during market hours 
+              (datetime.datetime.now().time().isoformat(timespec='seconds')) < Market_Close): 
+              repeat()
+              print("Moneytime")
+              r=threading.Thread(target=repeat(), name = 'test')
+              time.sleep(1)
+          
         else:
             print('The Market is Closed')
             time.sleep(2)
