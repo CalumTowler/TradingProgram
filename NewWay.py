b165ab1410c86aef2dbf8591382f1ea48830f72c
Admin <alex.sav@hotmail.co.uk>
2020-08-06 00:51:49 +0100

    New Data Puller using UI webscraper from Trading View Will program scraper into code later Added Function Analysis Working MA, RSI, MACD Analysis Added BB function and efficency saving Added multiple chart periods and selection of historical data Added rseistance and support point input and calculation current time is 0.36seconds(after inputs) + 30 second data collection Added day chart fixer file as day data lagged one day behind Finished R&S function Added fib function Expanded MACD function

--- NewWay.py
+++ NewWay.py
@@ -15,7 +15,9 @@ import math
 # pd.set_option('display.max_colwidth', -1)
 
 path = r'C:\Users\Admin\OneDrive\Oracle\Trading Program\Stock Data'
-listdf = {1:1,2:5,3:15,4:60,5:240,6:'1D'}
+listdf = {1:1,2:5,3:15,4:60,5:240,6:'1D',7:'1W'}
+
+
 
 def dffix(list,x,tp):
     excel1 = path + "\TVC_USOIL, " + str(list[x]) + ".csv"
@@ -28,14 +30,15 @@ def dffix(list,x,tp):
     df['time'] = pd.to_datetime(df['time'])  # changes time column format to datetime
     df = df.iloc[::-1] # revereses index
     df = df.reset_index(drop=True)  # reset so newest data is at index 0
-    tp = (tp-1) # tp is time period back selected -1 is used because index begins at 0
     if tp >= 0: # if 0 is selected as start then removing rows will be skipped
         df = df.drop(df.index[:tp]) #drops range of rows not wanted to make new df starting from point selected
         df = df.reset_index(drop=True) #reset index
     else:
         pass
     print('Current time is ' + str(df.loc[df.index[0], 'time']))
+
     return df
+
 
 #rsi checker where p = probability of upwards movement
 
@@ -63,11 +66,7 @@ def rsi_checker(rsi):
 
     return p
 
-#rsi_checker(currentrsi)
 
-
-
-#establishes current trend by determining MA crosses
 
 def trend_strength(MA25, MA50, MA100, MA200):
     print('MAs point to')
@@ -97,12 +96,6 @@ def trend_strength(MA25, MA50, MA100, MA200):
     #print(p)
     return p
 
-#trend_strength(cMA25, cMA50, cMA100, cMA200)
-
-#macd stuff
-
-
-
 def macd_checker(Histogram,df,time):
     #use histogram value to determine if macd is crossed up or down
     #checking when last macd cross was
@@ -110,8 +103,7 @@ def macd_checker(Histogram,df,time):
     if Histogram>0:
         print('MACD is crossed up')
         p=0.5
-        rslt_df = df[df[
-                         'Histogram'] < 0]  # finds positive histogram values to determine cross time (need to make it so it finds first value only and not make
+        rslt_df = df[df['Histogram'] < 0]  # finds positive histogram values to determine cross time (need to make it so it finds first value only and not make
         # massive df of all positive values  )
 
         # print(rslt_df)
@@ -119,8 +111,13 @@ def macd_checker(Histogram,df,time):
         # need to put this as outside function returns msot recent timestamp
         timed = dn - dt
         print('MACD cross occured ' + str(timed) + ' minutes ago')
+        gradient = (fval(df, 'Histogram', 0) - fval(df, 'Histogram',2)) / 3  # calculates gradient to determine if downtrend is increasing or decreasing
+        if gradient > 0:
+            print('MACD uptrend is increasing')
+        else:
+            print('MACD uptrend is decreasing')
 
-        rslt_df = df[df['Histogram']<0]
+
 
 
 
@@ -135,7 +132,11 @@ def macd_checker(Histogram,df,time):
          # need to put this as outside function returns msot recent timestamp
         timed = dn-dt
         print('MACD cross occured '+ str(timed) + ' minutes ago')
-
+        gradient = (fval(df,'Histogram',0)-fval(df,'Histogram',2))/3 #calculates gradient to determine if downtrend is increasing or decreasing
+        if gradient < 0:
+            print('MACD downtrend is increasing')
+        else:
+            print('MACD downtrend is decreasing')
 
     return p
 
@@ -160,6 +161,67 @@ def bb_checker(df):
     p=0
     return p
 
+
+def RS(df,cprice):
+    RS = []  # empty resistance point list
+    RS1 = input('What are the Resistance and support points')
+    while RS1 != "":  # if no input given then question is skipped
+        RS.append(RS1)
+        RS1 = input('What are the Resistance and Support points')
+    RS = list(map(float, RS))  # converts input values into floats
+
+    print('Resistance and Support points selected are' + str(RS))
+
+    rten = float(round(cprice, -1))  # founds rounded multiple of 10
+    cr = float(math.ceil(fval(df, 'close', 0)))  # finds cloeset dollar value above
+    cs = float(math.floor(fval(df, 'close', 0)))  # closest dolar value below
+
+    if rten > cprice:  # founded other multiple of 10 and 5 depending if first rounded number was a support or resistance
+        rfive = rten - 5
+        sten = rten - 10
+    else:
+        rfive = rten + 5
+        sten = rten + 10
+
+    RS.extend((cr, cs, rten, rfive, sten))
+    RS = list(dict.fromkeys(RS))  # removes any duplicates from inputs
+    R = [x for x in RS if x > cprice]  # seperates into R and s and reorders lists
+    S = [x for x in RS if x < cprice]
+
+    print('Support points are ' + str(S))
+    print('Resistance points are ' + str(R))
+    return R, S
+
+#fibonacci retracement
+
+def fib(interval):
+
+    if interval<4:
+        df=dffix(listdf,6,1)
+    else:
+        df=dffix(listdf,7,1)
+
+
+    high = fval(df, 'high', 0)
+    low = fval(df, 'low',0)
+    close = fval(df, 'close',0)
+    pp=round((high+low+close)/3,2)
+
+    flevels=[0.382,0.618,1.0]
+    SF = []
+    RF=[]
+    for x in flevels:
+
+        rf=round((pp+((high-low)*x)),2)
+        RF.append(rf)
+
+        sf = round((pp-((high-low)*x)),2)
+        SF.append(sf)
+
+    print(pp, RF,SF)
+    return(pp,RF,SF)
+
+
 #INPUT AND ACTIONS
 
 tp = int(input('How many periods ago would you like to start from?'))
@@ -170,53 +232,9 @@ df=dffix(listdf,1,tp)
 cprice = (fval(df,'close',0))
 print('Current price is ' + str(cprice))
 
-
-
 #inputting resistance and support points will make into fucntion
 
-RS = [] #empty resistance point list
-RS1 = input('What are the Resistance and support points')
-while RS1 !="": #if no input given then question is skipped
-    RS.append(RS1)
-    RS1 = input('What are the Resistance and Support points')
-
-RS = list(map(float, RS)) #converts input values into floats
-
-print('Resistance and Support points selected are' + str(RS))
-
-rten = float(round(cprice,-1)) #founds rounded multiple of 10
-
-cr = float(math.ceil(fval(df,'close',0))) #finds cloeset dollar value above
-
-cs = float(math.floor(fval(df,'close',0))) #closest dolar value below
-
-if rten>cprice: #founded other multiple of 10 and 5 depending if first rounded number was a support or resistance
-    rfive = rten-5
-    sten = rten-10
-else:
-    rfive =rten+5
-    sten = rten+10
-
-
-RS.extend((cr,cs,rten,rfive, sten))
-
-RS = list(dict.fromkeys(RS)) #removes any duplicates from inputs
-
-R = [x for x in RS if x>cprice] #seperates into R and s and reorders lists
-S = [x for x in RS if x<cprice]
-
-print('Support points are '+ str(S))
-print('Resistance points are ' + str(R))
-
-
-
-
-
-
-
-
-
-
+R,S = RS(df,cprice)
 
 t = input('Would you like to see all time periods? y/n')
 print(''
@@ -233,6 +251,8 @@ if t == 'y':
         print('Chart Interval is ' + (str(listdf[x])))
         #print('Current price is ' + str(fval(df,'close',0)))
         # takes most recent data point
+        fib(x)
+        print('hey')
         currentrsi = fval(df,'RSI',0)
         cMA25 = fval(df,'25MA',0)
         cMA50 = fval(df,'50MA',0)
@@ -255,6 +275,7 @@ else:
     print('Chart Interval is ' + (str(listdf[t])))
     print('Current price is ' + str(fval(df, 'close', 0)))
     # takes most recent data point
+    #fib(t)
     currentrsi = fval(df, 'RSI', 0)
     cMA25 = fval(df, '25MA', 0)
     cMA50 = fval(df, '50MA', 0)
@@ -263,6 +284,7 @@ else:
     chistogram = fval(df, 'Histogram', 0)
     p = rsi_checker(currentrsi) + trend_strength(cMA25, cMA50, cMA100, cMA200) + macd_checker(chistogram, df,
                                                                                               dn) + bb_checker(df)
+
     print('Trend probability is ' + str(p))
     print(''
           )
