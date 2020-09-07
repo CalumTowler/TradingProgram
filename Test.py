030708556ec70e780374c17d3f584de22022df27
Admin <alex.sav@hotmail.co.uk>
2020-09-07 21:16:05 +0100

    testing over last year

--- Test.py
+++ Test.py
@@ -22,6 +22,11 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', -1)
 
+def fval(df,column,val):
+    value = (df.loc[df.index[val], column])
+    return value
+
+
 def dffix(list,x,tp,ticker):
 
     excel1 = path + ticker + str(list[x]) + ".csv"
@@ -70,34 +75,81 @@ def trader(ticker):
     rsip = path + ticker + "short" + "Sep" + "rsiprob" + "60" + ".csv"
 
     dfrsip = pd.read_csv(rsip)
-    dfrsip=dfrsip[dfrsip["Value Change"]==1.5]
-    dfrsipup = dfrsip[dfrsip["Probability Up"]>0.75]
-    dfrsipdown =dfrsip[dfrsip["Probability Down"]>0.75]
+    dfrsip=dfrsip[dfrsip["Value Change"]==1]
+    dfrsipup = dfrsip[dfrsip["Probability Up"]>0.7]
+    dfrsipup = dfrsipup.reset_index(drop=True)
+
+    dfrsipdown =dfrsip[dfrsip["Probability Down"]>0.7]
+    dfrsipdown = dfrsipdown.reset_index(drop=True)
+
     print(dfrsipup)
     print(dfrsipdown)
 
     bbp = path + ticker + "short" + "Sep" + "bbprob" + "60" + ".csv"
 
     dfbbp = pd.read_csv(bbp)
-    dfbbp = dfbbp[dfbbp["Value Change"] == 1.5]
+    dfbbp = dfbbp[dfbbp["Value Change"] == 1]
     dfbbpup = dfbbp[dfbbp["Probability Up"] > 0.75]
+    dfbbpup = dfbbpup.reset_index(drop=True)
+
     dfbbpdown = dfbbp[dfbbp["Probability Down"] > 0.75]
+    dfbbpdown = dfbbpdown.reset_index(drop=True)
+
     print(dfbbpup)
     print(dfbbpdown)
 
-    dfticker=dffix(listdf, 4, 0, ticker)
+    dftickerfile = path + ticker + "short" + "60" + ".csv"
+    dfticker = pd.read_csv(dftickerfile)
+    dfticker['time'] = pd.to_datetime(dfticker['time'])
     dfticker["timedate"]=0
+    print(dfticker)
     for x in range(len(dfticker)): #remove current day from experiment
         dfticker.loc[dfticker.index[x], "timedate"]=(dfticker.loc[dfticker.index[x], "time"].date())
     currentdate = dfticker.loc[dfticker.index[0],"timedate"]
     dfticker=dfticker[dfticker["timedate"]!=currentdate]
     dfticker = dfticker.reset_index(drop=True)
 
-    newdate=currentdate - timedelta(days=6)
-    print(newdate)
 
-    dfcurrentday = dfticker[dfticker["timedate"]==newdate]
-    dfcurrentday = dfcurrentday.reset_index(drop=True)
+    for x in reversed(range(1,50)):
+        newdate=currentdate - timedelta(days=x)
+        dfcurrentday = dfticker[dfticker["timedate"]==newdate]
+        if len(dfcurrentday)!=0:
+            dfcurrentday = dfcurrentday.reset_index(drop=True)
+            for x in range(len(dfcurrentday)):
+                rsi=dfcurrentday.loc[dfcurrentday.index[x], "RSI"]
+                rsigrad=dfcurrentday.loc[dfcurrentday.index[x], "rsigrad"]
+                spreadgrad = dfcurrentday.loc[dfcurrentday.index[x], "Spread Grad"]
+                spreadratio=dfcurrentday.loc[dfcurrentday.index[x], "Spread Ratio"]
+                if fval(dfcurrentday, 'Upper', x) < fval(dfcurrentday, 'close', x):
+                    breakbb = "breakover"
+                elif fval(dfcurrentday, 'Lower', x) > fval(dfcurrentday, 'close', x):
+                    breakbb = "breakunder"
+                else:
+                    breakbb = "within"
+                if spreadgrad<0:
+                    stsq = "st"
+                else:
+                    stsq = "sq"
+                for y in range(len(dfrsipup)):
+                    y = dfrsipup.loc[dfrsipup.index[y], "RSI Range"].split(maxsplit=-1)
+                    z = dfrsipup.loc[dfrsipup.index[y], "RSI Gradient"].split(maxsplit=-1)
+                    if float(y[0]) < rsi < float(y[1]) and float(z[0]) < rsigrad < float(z[1]):
+                        print(dfrsipup.loc[y])
+                        print(fval(dfcurrentday, 'close', x))
+                    else:
+                        print("nope")
+                for z in range(len(dfbbpup)):
+                    y = dfbbpup.loc[dfbbpup.index[z], "bbprofile"].split(maxsplit=-1)
+                    if y[0]==breakbb and y[1]==stsq and float(y[2]) < spreadratio < float(y[3]):
+                        print(dfbbpup.loc[z])
+                        print(fval(dfcurrentday, 'close', x))
+                    else:
+                        print("nope")
+
+
+
+        else:
+            continue
     print(dfcurrentday)
     for x in range(len(dfcurrentday)):
         rsi=dfcurrentday["RSI"]
