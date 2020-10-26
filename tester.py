75085eb1e77c5d0ad4acada0017d30f97f548634
Admin <alex.sav@hotmail.co.uk>
2020-10-26 03:17:28 +0000

    starting momentum analysis

---
+++ tester.py
@@ -0,0 +1,4 @@
+cMA25 = float(df.loc[df.index[1], '25MA'])
+    cMA50 = float(df.loc[df.index[1], '50MA'])
+    cMA100 = float(df.loc[df.index[1], '100MA'])
+    cMA200 = float(df.loc[df.index[1], '200MA'])
